7bf1552a3018d73474a430382cb60bb1b289d934
ShashankRawat177 <shashankrawat177@gmail.com>
2026-06-09 18:03:17 +0530

    .

--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,1 +1,28 @@
-export class Comment {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
+
+import { User } from '../../users/entities/user.entity';
+import { Post } from '../../posts/entities/post.entity';
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column('text')
+  content!: string;
+
+  @ManyToOne(() => User)
+  author!: User;
+
+  @ManyToOne(() => Post)
+  post!: Post;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
